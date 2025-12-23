193810f833a9e2ec8e3297a1fc16817d59f5fcf3
xXJoaoVictorNogXx <joaovictorfcantanheden@gmail.com>
2025-12-23 18:16:49 -0300

    Ajuste de header

--- app/acoes/page.tsx
+++ app/acoes/page.tsx
@@ -14,33 +14,7 @@ export default function acoes() {
         </div>
       </section>
       <section className="w-full bg-white">
-        <div className="m-auto font-sans w-3/4 p-20 flex flex-col gap-4">
-          <h2 className="text-4xl font-bold text-purple-900 mb-8 text-left">
-            Nossas Ações
-          </h2>
-          <p>
-            Estamos comprometidos em fazer a diferença em nossa comunidade
-            através de diversas ações sociais. Acreditamos que, ao apoiar causas
-            importantes, podemos contribuir para um futuro melhor para todos.
-          </p>
-          <ul className="list-disc list-inside text-purple-900 text-lg space-y-2">
-            <li className="flex items-center gap-2">
-              <MapPin className="text-amber-400" />
-              Educação para Todos: Apoiamo projetos que visam melhorar o acesso
-              à educação em comunidades carentes.
-            </li>
-            <li className="flex items-center gap-2">
-              <MapPin className="text-amber-400" />
-              Sustentabilidade Ambiental: Participamos de iniciativas que
-              promovem a preservação do meio ambiente.
-            </li>
-            <li className="flex items-center gap-2">
-              <MapPin className="text-amber-400" />
-              Inclusão Digital: Fornecemos acesso à internet para comunidades
-              que não possuem essa infraestrutura.
-            </li>
-          </ul>
-        </div>
+        <div className="m-auto font-sans w-3/4 p-20 flex flex-col gap-4"></div>
       </section>
     </main>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,49 @@
+import { Card } from "@/components/ui/card";
 import Image from "next/image";
 
 export default function Home() {
+  const teste = [
+    {
+      title: "Transparência e Credibilidade",
+      description:
+        "A transparência e credibilidade são dois grandes pilares da nossa empresa. O bom caráter na condução dos processos geram confiança e respeito entre empresa, clientes e comunidade.",
+    },
+    {
+      title: "Foco no Cliente",
+      description:
+        "O foco no cliente está presente em todos os nossos processos. Por esse motivo entendemos que o nosso papel é desenvolver produtos e serviços que atendam as expectativas e estejam de acordo com os desejos e necessidades de cada usuário de nossos serviços.",
+    },
+    {
+      img: "",
+      title: "Espírito de Colaboração",
+      description:
+        "A atuação cooperativa faz com que os resultados tenham maior solidez. O espírito de colaboração prevalece sobre as ações individuais fortalece as empresas, estimula e valoriza as parcerias e aumenta os resultados pessoais e do grupo.",
+    },
+    {
+      title: "Espírito de Liderança",
+      description:
+        "O espírito de liderança permite adotar uma atitude voltada para a inovação e tecnologia de ponta como pontos de grande importância. Na sua interação com o mercado e as comunidades locais, sempre privilegiando a excelência em todas as ações.",
+    },
+  ];
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        <section>
+          <h2>AUTOATENDIMENTO</h2>
+          <div className="w-full grid grid-cols-4 gap-6 mt-10">
+            {teste.map((item, index) => (
+              <Card key={index} className="w-full text-center bg-purple-900">
+                <div className="flex flex-col gap-2 p-6 items-center">
+                  <h2 className="text-2xl font-semibold text-amber-400">
+                    {item.title}
+                  </h2>
+                  <p className="text-amber-50">{item.description}</p>
+                </div>
+              </Card>
+            ))}
+          </div>
+        </section>
       </main>
     </div>
   );
